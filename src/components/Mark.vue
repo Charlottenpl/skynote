62ee276c065de547ff2c1dfc71b80c5037e579df
chenzhongyou <33644882+Charlottenpl@users.noreply.github.com>
2022-08-30 17:20:53 +0800

    参考

--- src/components/Mark.vue
+++ src/components/Mark.vue
@@ -1,33 +1,20 @@
 <template>
-  <div>
-    <!--    渲染-->
-    <div class="markdown-body">
-<!--      <markdown/>-->
-<!--      {{mdVal}}-->
-    </div>
 
     <!--    编写-->
     <div>
-      <VueMarkdown :source="mdVal"></VueMarkdown>
+      <v-md-editor v-model="mdVal" height="400px"></v-md-editor>
     </div>
-  </div>
 </template>
 
 <script>
-// 引入 markdown 文件，引入后是一个组件，需要在 components 中注册
-// import markdown from '@/assets/Hello.md'
-// 代码高亮
-import 'highlight.js/styles/github.css'
-// 其他元素使用 github 的样式
-import 'github-markdown-css'
-// 组件
-import VueMarkdown from 'vue-markdown'
+//TODO https://code-farmer-i.github.io/vue-markdown-editor/zh/examples/base-editor.html
+//TODO https://blog.csdn.net/weixin_45291937/article/details/125275996?spm=1001.2101.3001.6650.7&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-7-125275996-blog-126064438.t5_layer_eslanding_D_0&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7ERate-7-125275996-blog-126064438.t5_layer_eslanding_D_0&utm_relevant_index=8
+
 
 export default {
   name: "Mark",
   components: {
-    VueMarkdown, //注册组件
-    // markdown,
+
   },
   data() {
     return {
